63745213fb66f08e8c38c5b6234cb37e0c75ede9
elie-aigon <elie.aigon@laplateforme.io>
2023-01-19 12:02:30 +0100

    pfiouu

--- test.py
+++ test.py
@@ -10,6 +10,17 @@ def input_tuple():
     new_tuple = (heure, minute, sec)
 
     return new_tuple
+
+def input_tuple_us():
+    string = input('Entrez des entiers tel que : hh mm ss XX ou XX représente am ou pm: ')
+    string_tuple = tuple(string.split())
+    sec = int(string_tuple[2])
+    minute = int(string_tuple[1])
+    heure = int(string_tuple[0])
+    am_pm = string_tuple[3]
+    new_tuple = (heure, minute, sec, am_pm)
+
+    return new_tuple
 def set_heure():
     modif = input('Voulez vous modifier l heure actuelle? Y/N : ')
     if modif.lower() == "y":
@@ -30,14 +41,16 @@ def horloge(random_tuple):
     sec = int(random_tuple[2])
     minute = int(random_tuple[1])
     heure = int(random_tuple[0])
-    alarme_yesno = input('Voulez vous set une arlame? Y/N: ')
-    if alarme_yesno.lower() == 'y':
-        print('Alarme:  ')
-        alarm_tuple = input_tuple()
-    else:
-        alarm_tuple = 0
+
+
     mode = input('mode 24 ou 12: ')
     if mode == "24":
+        alarme_yesno = input('Voulez vous set une arlame? Y/N: ')
+        if alarme_yesno.lower() == 'y':
+            print('Alarme:  ')
+            alarm_tuple = input_tuple()
+        else:
+            alarm_tuple = 0
         print('maintenez - p - pour mettre pause')
         while True:
             if keyboard.is_pressed('p'):
@@ -63,6 +76,12 @@ def horloge(random_tuple):
             if minute == 30 and sec == 0:
                 set_heure()
     elif mode == "12":
+        alarme_yesno = input('Voulez vous set une arlame? Y/N: ')
+        if alarme_yesno.lower() == 'y':
+            print('Alarme:  ')
+            alarm_tuple = (input_tuple_us())
+        else:
+            alarm_tuple = 0
         if heure > 11:
             am_pm = "PM"
         else:
@@ -94,6 +113,7 @@ def horloge(random_tuple):
                     am_pm = "PM"
             time.sleep(1)
             temps = (heure, minute, sec, am_pm)
+
             print(heure, ':', minute, ':', sec, '-', am_pm)
             check_alarm(alarm_tuple, temps)
             if minute == 30 and sec == 0:
